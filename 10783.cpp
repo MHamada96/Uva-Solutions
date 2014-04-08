c0d28874e0e78c78720ece6346198b1dab19a599
asimali246 <asimali246@yahoo.com>
2014-04-08 18:21:23 +0530

    Create 10783.cpp

---
+++ 10783.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <set>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <list>
+#include <string>
+#include <vector>
+#include <new>
+#include <bitset>
+#include <ctime>
+#include <stdint.h>
+#include <unistd.h>
+ 
+ using namespace std;
+ 
+#define ll long long int
+#define INF 1000000000
+#define PI acos(-1.0)
+#define EPS 1e-9
+ 
+template <typename X> X gcd(X a, X b){if(!b)return a; else return gcd(b, a%b);}
+ 
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> llp;
+typedef pair<double, double> dd;
+typedef pair<char, int> ci;
+
+int t, a, b, ans, cs=1, i;
+
+int main()
+{
+    scanf("%d", &t);
+    while(t--)
+    {
+        ans=0;
+        scanf("%d %d", &a, &b);
+        for(i=a;i<=b;++i)
+            if(i%2)
+                ans+=i;
+        printf("Case %d: %d\n", cs++, ans);
+    }
+    return 0;
+}
